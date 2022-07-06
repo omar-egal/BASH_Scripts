0f77d9f6d808b5a8ec9d5a3206c7ba2d0d2cc2f0
EC2 Default User <ec2-user@ip-172-31-92-175.ec2.internal>
2022-07-06 19:33:00 +0000

    Modified user.sh to include prompt for user password

--- user.sh
+++ user.sh
@@ -5,13 +5,16 @@ echo "Please enter a new user(s): "
 
 read -a users
 
+wait
+read -s -p "Please enter password: " password
+echo
 for user in ${users[@]}; do
         if id -u $user > /dev/null 2>&1; then
                 echo "The username $user is already taken, please enter a different username"
 
         else
-                sudo useradd -m "$user"
+                pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+                sudo useradd -m -p "$pass" "$user"
                 [ $? -eq 0 ] && echo "$user was successfully added."
         fi
 done
-
